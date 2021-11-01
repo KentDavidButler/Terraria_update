30741503493642ab48474cb40e683e9476aed352
Kent Butler <KentDavidButler@gmail.com>
2021-10-31 21:29:12 -0400

    Cleaned up troubleshooting code

--- Terraria_Website.py
+++ Terraria_Website.py
@@ -8,7 +8,6 @@ import re
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import time
-from selenium import webdriver
 from selenium.webdriver import Chrome
 
 
@@ -40,9 +39,9 @@ class TerrariaWebsite:
 
     def _get_website(self):
         # Note to self: Keep Webdriver on the same version as Chrome
-        driver = Chrome("G:\\KAdmin\\Pycharm\\Terraria_update\\chromedriver.exe")
+        driver = Chrome()
         driver.get(self.url)
-        time.sleep(5)
+        time.sleep(3)
         html = driver.page_source
         driver.quit()
 
@@ -52,23 +51,15 @@ class TerrariaWebsite:
         a_href = None
         site_info = self._get_website()
         soup = BeautifulSoup(site_info)
-        find_a_tags_by_class = soup.find_all("a", {"class": "external text"})
-        print(find_a_tags_by_class)
-        print("****")
-        a_tags_list = str(find_a_tags_by_class).split(',')
-        print(a_tags_list)
-        print("*****")
 
-        count = 0
+        find_a_tags_by_class = soup.find_all("a", {"class": "external text"})
+        a_tags_list = str(find_a_tags_by_class).split(',')
+
         for a_tag in a_tags_list:
-            count = count + 1
-            print(f"{count}: {a_tag}")
             if re.findall(r'terraria-server-\d{4}', a_tag):
                 a_href = a_tag
                 break
 
-        print(a_href)
-        print("******")
         return a_href
 
     def download_latest_version(self):
